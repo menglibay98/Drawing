e5591bf444bd6b82398baaab8b2c06d56f0d9fda
Ислам <islam@MacBook-Air-Islam.local>
2019-01-05 23:08:57 +0600

    Initial Commit

---
+++ Drawing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Drawing
+//
+//  Created by Ислам on 1/5/19.
+//  Copyright © 2019 Ислам. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
